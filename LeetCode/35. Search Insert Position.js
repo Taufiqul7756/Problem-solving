68a2f889550caa50101168905e471c85d4cde663
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-02-15 23:48:24 +0600

    35 - Search Insert Position

---
+++ LeetCode/35. Search Insert Position.js
@@ -0,0 +1,16 @@
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length;
+
+  while (left < right) {
+    const middle = Math.floor((left + right) / 2);
+
+    if (nums[middle] < target) {
+      left = middle + 1;
+    } else {
+      right = middle;
+    }
+  }
+
+  return left;
+};
